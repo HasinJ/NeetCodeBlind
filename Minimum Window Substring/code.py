d97b26bf8de74ba65d6c33849ff862154eb8f342
HasinJ <hasinchoudhuryj@gmail.com>
2022-11-06 12:20:42 -0500

    2nd attempt on longest 1

--- Minimum Window Substring/code.py
+++ Minimum Window Substring/code.py
@@ -17,32 +17,28 @@ def minWindow(s: str, t: str) -> str:
     hash = {}
 
     for right in range(len(s)):
-        print(right)
-        if (s[right] in t): # only do special things when a character in t has been found while moving right pointer
+        if (s[right] in t or s[right] in hash): # only do special things when a character in t has been found while moving right pointer
+
             if (s[right] in hash): hash[s[right]]+=1 #adding to hash
             else: hash[s[right]]=1 #adding to hash
+            t=t.replace(s[right],'',1) #removing it from needing to find
 
 
-
-            while len(hash) == len(t): #when all the letters in t has been found
-
-
-                if (s[left] in t):
+            while len(t)==0: #when all the letters in t has been found
+                if (len(s[left:right+1]) < len(shortest) or shortest==''): shortest = s[left:right+1] #record length when there is nothing to be found in t
+                if (s[left] in hash):
                     hash[s[left]]-=1 #only decrement a character from hash if it exists in t
-                    if (hash[s[left]] == 0): #if the count for the letter has reached 0, record the length
-                        if (len(s[left:right+1]) < len(shortest) or shortest==''): shortest = s[left:right+1]
-                        #print(s[left:right+1])
+                    if (hash[s[left]] == 0): #if the count for the letter has reached 0, remove from hash and insert back into t
                         hash.pop(s[left])
+                        t+=s[left]
 
                 left += 1 #move the left pointer
-
-
-
     return shortest
 
 #print(minWindow("ADOBECODEBANC","ABC"))
 #print(minWindow("a","a"))
 #print(minWindow("a","aa"))
-print(minWindow("abc","cba"))
+#print(minWindow("abc","cba"))
+print(minWindow("acbacca","aba"))
 #print(minWindow("a","b"))
 #print("abc" == "bca")
