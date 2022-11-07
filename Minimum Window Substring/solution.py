c6bdd5b8f11914ec29128b322fa6bd30a0e5fce3
HasinJ <hasinchoudhuryj@gmail.com>
2022-11-06 19:15:14 -0500

    before submitting

--- Minimum Window Substring/solution.py
+++ Minimum Window Substring/solution.py
@@ -22,26 +22,29 @@ def minWindow(s: str, t: str) -> str:
 
 
     for right in range(len(s)):
+        print(right, ":")
         if(s[right] in need):
             #update count
-            if ((s[right] in have)): have[s[right]]+=1 #adding to have
-            else: have[s[right]]=1 #adding to have
+            have[s[right]] = 1 + have.get(s[right], 0) #adding to have
+        print(have)
 
         #comparing what we just added to what we need
-        if (s[right] in need):
-            if(have[s[right]] == need[s[right]]): #if what we're adding's count is equivalent to what's in our need
-                accumulatedHave+=1
+        if (s[right] in need and have[s[right]] == need[s[right]]):
+            #if what we're adding's count is equivalent to what's in our need
+            accumulatedHave+=1
+            print("acchave:",accumulatedHave)
             #if we then have what we need, shrink left and update haves if necessary
-            while(accumulatedHave == accumulatedNeed):
-                if(right-left+1 < length):
-                    length = right-left+1
-                    result = [left, right]
-                    print(left, right)
+        while(accumulatedHave == accumulatedNeed):
 
-                if(s[left] in have):
-                    have[s[left]]-=1
-                    if(have[s[left]] != need[s[left]]): accumulatedHave-=1
-                left+=1
+            if(right-left+1 <= length):
+                length = right-left+1
+                result = [left, right]
+                print(left, right)
+
+            if(s[left] in need):
+                have[s[left]]-=1
+                if(have[s[left]] < need[s[left]]): accumulatedHave-=1
+            left+=1
             #print(s[result[0]:result[1]+1])
 
 
@@ -50,10 +53,10 @@ def minWindow(s: str, t: str) -> str:
 
 
 
-print(minWindow("ADOBEC","ABC"))
+print(minWindow("ADOBECODEBANC","ABC"))
 #print(minWindow("a","a"))
 #print(minWindow("a","aa"))
 #print(minWindow("abc","cba"))
-#print(minWindow("acbacca","aba"))
+print(minWindow("acbacca","aba"))
 #print(minWindow("a","b"))
 #print("abc" == "bca")
