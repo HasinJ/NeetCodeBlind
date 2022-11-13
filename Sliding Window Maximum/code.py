70d73b42b1b4f9f1a9b7373b4237b3c275e654c8
HasinJ <hasinchoudhuryj@gmail.com>
2022-11-13 15:39:23 -0500

    before giving up

--- Sliding Window Maximum/code.py
+++ Sliding Window Maximum/code.py
@@ -1,7 +1,17 @@
 
-
+from collections import deque
 
 def maxSlidingWindow(nums, k: int):
     print("nums:", nums, "k:", k)
+    d = deque()
+
+    for i in range(len(nums)):
+
+        if d:
+            while nums[i] < d[0]:
+                d.popleft()
+                print(d, "popping")
+        d.append(nums[i])
+        k-=1
 
 maxSlidingWindow([1,3,-1,-3,5,3,6,7], 3)
