291b0a228386d055c0648a32a69b1dd782336f14
HasinJ <hasinchoudhuryj@gmail.com>
2022-11-28 18:42:51 -0500

    final solution

--- Product of Array Except Itself/code.py
+++ Product of Array Except Itself/code.py
@@ -1,41 +1,15 @@
 def productExceptSelf(nums):
-    res = []
-    print(nums)
+    res = [1] * (len(nums))
 
-    prefix=[]
+    prefix = 1
     for i in range(len(nums)):
-        if i==0:
-            prefix.append(nums[i])
-            continue
-        newnum = nums[i]*prefix[i-1]
-        prefix.append(newnum)
-
-    postfix=[]
-    for i in range(len(nums)):
-        postfix.append(0)
-    for i in reversed(range(len(nums))):
-        if i==len(nums)-1:
-            postfix[i] = nums[i]
-            continue
-        newnum = nums[i]*postfix[i+1]
-        postfix[i] = newnum
-
-    for i in range(len(nums)):
-        if i == 0:
-            res.append(postfix[i])
-            continue
-        if i == len(nums)-1:
-            res.append(prefix[i-1])
-            continue
-        
-    print(prefix)
-    print(postfix)
-
-    #for i in range(len(nums)):
-
-
-
-
+        res[i] = prefix
+        prefix *= nums[i]
+    postfix = 1
+    for i in range(len(nums) - 1, -1, -1):
+        res[i] *= postfix
+        postfix *= nums[i]
     return res
+
 
 print(productExceptSelf([1,2,3,4]))
