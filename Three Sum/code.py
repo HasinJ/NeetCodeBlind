2a40636f33f84992a67ae3b803b600608bbfa8b1
HasinJ <hasinchoudhuryj@gmail.com>
2023-01-20 18:00:51 -0500

    need to test for array of all the same nums

--- Three Sum/code.py
+++ Three Sum/code.py
@@ -1,6 +1,25 @@
 
 
 def threeSum(nums):
-    res = 
+    res = []
+    left, right = 0, len(nums)-1
+    nums.sort()
 
-threeSum([-1,0,1,2,-1,-4])
+    for curr in range(len(nums)-2):
+        left = curr+1
+        target = 0 - nums[curr]
+        while left<right:
+            sum = nums[left] + nums[right]
+            if sum == target:
+                if [nums[left], nums[right], nums[curr]] not in res:
+                    res.append([nums[left], nums[right], nums[curr]])
+                    break
+            elif sum < target:
+                left+=1
+            elif sum > target:
+                right-=1
+
+
+    return res
+
+print(threeSum([0,0,0,0]))
