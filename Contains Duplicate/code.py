2a4a4a646d79ee31a363f106bac1355ed98360c5
HasinJ <hasinchoudhuryj@gmail.com>
2023-03-08 21:30:33 -0500

    didnt copy correctly

--- Contains Duplicate/code.py
+++ Contains Duplicate/code.py
@@ -3,6 +3,6 @@ def containsDuplicate(nums):
     for i in range(len(nums)):
        if nums[i] not in unique: unique.add(nums[i])
        else: return True
-    return Fals
+    return False
 
 print(containsDuplicate([1,2,3,1]))
